8fa95978fc5c47fd51f0dbbf3e432f0bf5eceb10
Wu Chenhua <chanhuawu@outlook.com>
2025-04-08 20:10:58 +0800

    feat: 添加新的HPC脚本和示例数据文件
    新增多个HPC脚本用于运行不同的任务，包括sc_multiome和sc_rna的实验配置。同时添加了示例数据文件和用于绘制评估指标的Jupyter笔记本。

---
+++ src/run.hpc.fit.sc_mul.s42.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#JSUB -J deeptan_sc_multiome_s42
+#JSUB -q gpu
+#JSUB -n 1
+#JSUB -gpgpu '1 mig=1'
+#JSUB -o log_out.%J
+#JSUB -e log_err.%J
+
+module purge
+module load singularity-4.2.1
+
+MY_HOME=/storage/public/home/2022051346
+DEEPTAN_HOME=$MY_HOME/prj/deeptan
+SIF=$DEEPTAN_HOME/deeptan_20250408.sif
+myscript=run_05_fit_tune.py
+
+optdata=sc_multiome_minmi0.35_top2000
+folder=seed_42
+task_name=multitask
+ntrial=1
+njob=1
+bs=16
+agd=2
+ck=512
+
+dirlitdata_t=$DEEPTAN_HOME/data/optimized_data/$optdata/$folder
+dirlitdata_v=/mnt/litdata
+
+dirlogs_t=$DEEPTAN_HOME/run/logs
+dirlogs_v=/mnt/litlogs
+dirlogs=$dirlogs_v/$optdata/$folder/$task_name
+
+singularity exec \
+--bind $dirlitdata_t:$dirlitdata_v,$dirlogs_t:$dirlogs_v \
+--nv $SIF sh -c "export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True && mkdir -p $dirlogs && python $myscript --data $dirlitdata_v --bs $bs --logdir $dirlogs --nt $ntrial --nj $njob --agb $agd --ck $ck --atune"
--- src/run.hpc.fit.sc_rna.s44.sh
+++ src/run.hpc.fit.sc_rna.s44.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#JSUB -J deeptan_sc_rna_annotated_s42
+#JSUB -J deeptan_sc_rna_annotated_s44
 #JSUB -q gpu
-#JSUB -n 2
+#JSUB -n 1
 #JSUB -gpgpu '1 mig=1'
 #JSUB -o log_out.%J
 #JSUB -e log_err.%J
@@ -11,12 +11,13 @@ module load singularity-4.2.1
 
 MY_HOME=/storage/public/home/2022051346
 DEEPTAN_HOME=$MY_HOME/prj/deeptan
-SIF=$DEEPTAN_HOME/deeptan.sif
+SIF=$DEEPTAN_HOME/deeptan_20250408.sif
 myscript=run_05_fit_tune.py
 
-optdata=sc_rna_annotated
-folder=seed_42
-ntrial=20
+optdata=sc_rna_annotated_minmi0.0_top2000
+folder=seed_44
+task_name=multitask
+ntrial=1
 njob=1
 bs=16
 agd=2
@@ -27,7 +28,7 @@ dirlitdata_v=/mnt/litdata
 
 dirlogs_t=$DEEPTAN_HOME/run/logs
 dirlogs_v=/mnt/litlogs
-dirlogs=$dirlogs_v/$optdata/$folder
+dirlogs=$dirlogs_v/$optdata/$folder/$task_name
 
 singularity exec \
 --bind $dirlitdata_t:$dirlitdata_v,$dirlogs_t:$dirlogs_v \
